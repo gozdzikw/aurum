b5d84cb587c004fc00b00bcd1cd2e39a3c87a5b0
Wiola Gozdzik <wgozdzik@192.168.1.12>
2025-09-27 16:51:03 +0200

    init commit

---
+++ src/test/java/com/mybank/aurum/AurumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybank.aurum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
